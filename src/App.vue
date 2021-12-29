1750144db0d46e37a02e30f6261a20924753148b
Desi <desislava2810@abv.bg>
2021-12-29 17:41:38 +0200

    feat: Use vue-test-packageeee-dgp component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Test/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Test from 'vue-test-packageeee_dgp'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Test
   }
 }
 </script>
